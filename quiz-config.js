36841a878c24449a2c893c2d29686422c4dfa38c
Panagiotis Konidis <pkonidis@more.com>
2025-07-21 12:26:39 +0300

    Commit 3x3 Puzzle resize & Fix Question

--- quiz-config.js
+++ quiz-config.js
@@ -1,29 +1,29 @@
 window.QUIZ_CONFIG = {
     questions: [
         {
-            question: "How many players are the National Teams registering in this competition?",
-            options: ["9", "12", "14", "16"],
-            correct: 1
-        },
-        {
-            question: "What is Giannis Antetokounmpo's jersey number?",
-            options: ["23", "10", "35", "95"],
+            question: "What do the initials FIBA stand for?",
+            options: ["Federation of Island Basketball Athletes", "Football International Body Association", "Fédération Internationale de Basketball", "Final International Basketball Alliance"],
             correct: 2
         },
         {
-            question: "Which National team holds the record for the most wins in FIBA Competitions?",
-            options: ["USA", "Serbia", "Greece", "Spain"],
+            question: "How many points is a successful basket worth when a player scores from behind the three-point line?",
+            options: ["3", "2", "1", "4"],
             correct: 0
         },
         {
-            question: "who invented the basketball hoop?",
-            options: ["Larry Bird", "John Logie Baird", "James Naismith", "Benjamin Franklin"],
+            question: "Which of the following countries is NOT included in Group C of the FIBA EuroBasket qualifiers taking place in Cyprus? ",
+            options: ["Greece", "Bosnia and Herzegovina", "Germany", "Happy Fox"],
             correct: 2
         },
         {
-            question: "What is the largest mammal in the world?",
-            options: ["Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
+            question: "What is the name of the FIBA EuroBasket 2025 Mascot?",
+            options: ["FIBA Rabbit", "Marky Mark", "Fast Runner", "Benjamin Franklin"],
             correct: 1
+        },
+        {
+            question: "Which of the following is considered a violation in a basketball game?",
+            options: ["One-handed pass", "Dribbling with hand switch", "Traveling", "Jumping for a rebound"],
+            correct: 2
         }
     ]
 };
