d649c2fa00efe174eb377fb31e7300ef76881c81
Panagiotis Konidis <pkonidis@more.com>
2025-08-22 19:59:38 +0300

    Cache Image files for offline puzzle game play

--- sw.js
+++ sw.js
@@ -7,7 +7,16 @@ const urlsToCache = [
   './puzzle-icon.png',
   './ball-icon.png',
   './quiz-icon.png',
-  './back-icon.png'
+  './back-icon.png',
+  './images/1.jpg',
+  './images/2.jpg',
+  './images/3.jpg',
+  './images/4.jpg',
+  './images/5.jpg',
+  './images/6.jpg',
+  './images/7.jpg',
+  './images/8.jpg',
+  './images/9.jpg'
 ];
 
 self.addEventListener('install', function(event) {
